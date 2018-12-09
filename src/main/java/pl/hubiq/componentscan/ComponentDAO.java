d994ca4cc89f36fef68c0efc52410859b6f3b5e3
Hubert Świetlicki <hubert.swietlicki@gmail.com>
2018-12-09 15:10:54 +0100

    Scanning for components in different package.

---
+++ src/main/java/pl/hubiq/componentscan/ComponentDAO.java
@@ -0,0 +1,19 @@
+package pl.hubiq.componentscan;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ComponentDAO {
+
+    private final ComponentJdbcConnection componentJdbcConnection;
+
+    @Autowired
+    public ComponentDAO(ComponentJdbcConnection componentJdbcConnection) {
+        this.componentJdbcConnection = componentJdbcConnection;
+    }
+
+    public ComponentJdbcConnection getComponentJdbcConnection() {
+        return componentJdbcConnection;
+    }
+}
---
+++ src/main/java/pl/hubiq/hubspringtut1/HubSpringComponentScanApplication.java
@@ -0,0 +1,25 @@
+package pl.hubiq.hubspringtut1;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import pl.hubiq.componentscan.ComponentDAO;
+
+@SpringBootApplication
+@ComponentScan("pl.hubiq.componentscan")
+public class HubSpringComponentScanApplication {
+
+    private static Logger LOGGER = LoggerFactory.getLogger(HubSpringComponentScanApplication.class);
+
+    public static void main(String[] args) {
+
+        ApplicationContext applicationContext = SpringApplication.run(HubSpringComponentScanApplication.class, args);
+
+        ComponentDAO componentDAO = applicationContext.getBean(ComponentDAO.class);
+
+        LOGGER.info("{}", componentDAO);
+    }
+}
