274a7ee989eb74cf112db8dedb9e1a980b438af5
Hubert Świetlicki <hubert.swietlicki@gmail.com>
2018-12-10 16:30:36 +0100

    PostConstruct & PreDestroy

--- src/main/java/pl/hubiq/hubspringtut1/basic/BinarySearchImpl.java
+++ src/main/java/pl/hubiq/hubspringtut1/basic/BinarySearchImpl.java
@@ -1,16 +1,20 @@
 package pl.hubiq.hubspringtut1.basic;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.config.ConfigurableBeanFactory;
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component
-@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class BinarySearchImpl {
 
     private final SortAlgorithm sortAlgorithm;
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     public BinarySearchImpl(@Qualifier("bubble") SortAlgorithm sortAlgorithm) {
@@ -21,5 +25,15 @@ public class BinarySearchImpl {
         int[] sortedNumbers = sortAlgorithm.sort(numbers);
         System.out.println(sortAlgorithm);
         return 3;
+    }
+
+    @PostConstruct
+    public void postConstruct() {
+        logger.info("postConstruct");
+    }
+
+    @PreDestroy
+    public void preDestroy() {
+        logger.info("preDestroy");
     }
 }
